479bc88b1542ef2e4c9bc1948a79bc735cb3913e
RizwanTariq <rizwantariq95@outlook.com>
2020-04-12 17:06:32 +0500

    JS-Sandbox

---
+++ js_sandbox/app.js
@@ -0,0 +1,71 @@
+// console.time("Block");
+// {
+//   console.error("This is a error");
+//   console.warn("This is a warning");
+//   console.info("This is the information");
+//   console.table({ a: 1, b: 2 });
+// }
+// console.timeEnd("Block");
+
+let val;
+
+// val = Number(null);
+// val = Number(undefined);
+// val = Number("string");
+
+// val = parseInt("100");
+// val = parseFloat("100.90");
+
+// console.log(val);
+// console.log(typeof val);
+
+// val = Math.PI;
+// val = Math.round(val);
+
+////STRINGS
+
+// let firstName = "Rizwan";
+// let lastName = "Tariq";
+// let fullName = firstName.concat(" ", lastName);
+
+// fullName = fullName.toUpperCase();
+
+// val = fullName.lastIndexOf("R");
+
+// val = fullName.charAt(9);
+
+// val = fullName.slice(0, 5);
+
+// val = fullName.slice(-4);
+
+////TEMPLATE LITERALS
+// const name = "Rizwan";
+// let html = `<ul>
+// <li>${name}</li>
+// </ul>`;
+// document.body.innerHTML = html;
+
+//ARRAYS
+
+// let array = [1, 2, 3, 4, 5];
+
+// array = new Array("Hasan", "Kanjar", "Hai", "And", "Fareed", "Bygarta", "hai");
+// val = array.join("-");
+
+// val = array.length;
+
+// val = Array.isArray(array);
+// val = array.pop();
+// val = array.shift();
+
+// console.log(myFunction());
+// console.log(yourFunction());
+
+// function myFunction() {
+//   console.log("myFunction");
+// }
+// const yourFunction = function () {
+//   console.log("yourFunction");
+// };
+
+console.log(val);
